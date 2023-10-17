faf8876beebd91f1fa1c46c1cd36acc561cc9d43
HalavicH <HalavicH@gmail.com>
2023-10-17 22:34:56 +0200

    python: Update script that formats text in the <text> tag

--- format-text-entries-in-xml.py
+++ format-text-entries-in-xml.py
@@ -1,7 +1,24 @@
 import sys
 
-import lxml.etree as ET
+import lxml.etree as etree
 import textwrap
+import re
+
+
+def indent(elem, level=0):
+    i = "\n" + level * "    "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "    "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level + 1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
 
 
 def format_text(text, indent):
@@ -9,7 +26,7 @@ def format_text(text, indent):
     text = ' '.join(text.split())
 
     # Step 2: Place line breaks before \n
-    text = text.replace(r'\n', '\n\n')
+    text = text.replace(r'\\n', '\n\\n')
 
     # Step 4: Wrap lines by word if longer than 85 char without inserting \n symbol
     lines = text.split('\n')
@@ -21,18 +38,19 @@ def format_text(text, indent):
     # Step 3: Indent everything according to the position of the <text> tag
     indented_lines = [indent + line for line in wrapped_lines]
 
-    return '\n'.join(indented_lines)
+    return '\n' + '\n'.join(indented_lines)
 
 
 def reformat_xml(xml_file):
-    parser = ET.XMLParser(remove_blank_text=True)  # This will help preserve the indentations
-    tree = ET.parse(xml_file, parser)
+    parser = etree.XMLParser(remove_blank_text=True)  # This will help preserve the indentations
+    tree = etree.parse(xml_file, parser)
     root = tree.getroot()
 
     for string_elem in root:
         for text_elem in string_elem:
             text_elem.text = format_text(text_elem.text, ' ' * 12)  # Assuming indent of 12 spaces
 
+    indent(root)
     # Write the reformatted XML to a new file
     tree.write(xml_file, encoding='windows-1251', xml_declaration=True, pretty_print=True)
 
