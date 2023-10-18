e655b8ad79c58ed436fbbf17a4fce88fbbae8a69
HalavicH <HalavicH@gmail.com>
2023-10-18 08:49:23 +0200

    python: Update script that formats text in the <text> tag

--- format-text-entries-in-xml.py
+++ format-text-entries-in-xml.py
@@ -1,9 +1,14 @@
-import sys
+import glob
 
+import colorama
 import lxml.etree as etree
 import textwrap
-import re
 
+from colorama import Fore
+
+colorama.init(autoreset=True)
+
+CURRENT_FILE_ISSUES = []
 
 def indent(elem, level=0):
     i = "\n" + level * "    "
@@ -47,7 +52,7 @@ def format_text(text, indent):
     return '\n' + '\n'.join(indented_lines) + '\n'
 
 
-def reformat_xml(xml_file):
+def process_file(xml_file):
     parser = etree.XMLParser(remove_blank_text=True)  # This will help preserve the indentations
     tree = etree.parse(xml_file, parser)
     root = tree.getroot()
@@ -61,7 +66,32 @@ def reformat_xml(xml_file):
     tree.write(xml_file, encoding='windows-1251', xml_declaration=True, pretty_print=True)
 
 
-# Call the function with your XML file
-# reformat_xml(sys.argv[1])
-reformat_xml(
-    '/Users/oleksandrkholiavko/IdeaProjects/stalker-gamma-0.9.1-ukr/gamedata/configs/text/eng/st_items_tools.xml')
+def main():
+    # Define the directory to search (change this to your directory)
+    directory = './**/*.xml'
+
+    failed_files = {}
+    # Get the list of all XML files in the specified directory and subdirectories
+    xml_files = glob.glob(directory, recursive=True)
+
+    # Perform the encoding roundtrip for each XML file
+    for file_path in xml_files:
+        try:
+            print(f'Processing {file_path}')
+            CURRENT_FILE_ISSUES.clear()
+            process_file(file_path)
+        except Exception as e:
+            print(Fore.RED + f"Error processing {Fore.RED + file_path}: {e}")
+            CURRENT_FILE_ISSUES.append(Fore.RED + f"Error: {e}" + Fore.RESET)
+            failed_files[file_path] = list(CURRENT_FILE_ISSUES)
+
+    print()
+    print("#" * 80)
+    print("\t\t\tFailed files:")
+    for file in failed_files:
+        print(f"\nFile: '{file}'")
+        for issue in failed_files[file]:
+            print("\t" + issue)
+
+if __name__ == "__main__":
+    main()
