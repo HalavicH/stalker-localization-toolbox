29a81a51f7db94f4a647a5dbfad40a66cacd95ce
HalavicH <HalavicH@gmail.com>
2023-10-19 19:09:56 +0200

    python: Improve encoding analysis

--- stalker-localization-scripts/standalone/analyze-encodings-and-list-suspicious.py
+++ stalker-localization-scripts/standalone/analyze-encodings-and-list-suspicious.py
@@ -9,7 +9,6 @@ Dependencies:
 Install dependencies using the following command:
 pip3 install chardet colorama prettytable
 """
-
 import argparse
 import glob
 import os
@@ -45,45 +44,87 @@ def detect_encoding(file_path):
     return result['encoding']
 
 
-def check_encoding(file_path):
-    thread_name = threading.current_thread().name
-    color = get_thread_color()
-    print(f"{color}[Thread {thread_name}] Processing file: {file_path}")
+def check_encoding(file_path, i):
+    # thread_name = threading.current_thread().name
+    # color = get_thread_color()
+    # print(f"{color}[Thread {thread_name}] Processing file #{i}: {file_path}")
+    # print(" " * 100,end='\r')
+    print(Fore.GREEN + f"  Processing file #{i}" ,end='\r')
     encoding = detect_encoding(file_path)
     if encoding.lower() not in ALLOWED_ENCODINGS:
-        return (file_path, encoding)
+        return (file_path, encoding, is_windows_1251_compatible(file_path))
     return None
+
+
+def is_windows_1251_compatible(file_path):
+    try:
+        # Attempt to decode the file with windows-1251 encoding
+        with open(file_path, 'rb') as file:
+            data = file.read()
+            decoded_text = data.decode('windows-1251')
+            return True  # Successfully decoded with windows-1251
+    except UnicodeDecodeError:
+        return False  # Decoding error occurred, not compatible with windows-1251
 
 
 def process_files(path):
     if os.path.isdir(path):
-        xml_files = glob.glob(f'{path}/**/**/*.xml', recursive=True)
+        xml_files = set(glob.glob(f'{path}/**/*.xml', recursive=True))
     else:
         xml_files = [path]
 
     total_files = len(xml_files)
-
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        results = list(executor.map(check_encoding, xml_files))
-
-    incorrect_encoding_files = [result for result in results if result is not None]
-
     print(f"Total files: {total_files}")
+
+    # futures = []
+    # with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    #     for i, xml_file in enumerate(xml_files):
+    #         # print(f"Submitted file [{i}]: {xml_file}")
+    #         futures.append(executor.submit(check_encoding, xml_file, i))
+    #     # results = list(executor.map(check_encoding, xml_files))
+    #
+    # incorrect_encoding_files = []
+    # for i in range(0, len(futures)):
+    #     # print(f"Waiting for future {i}")
+    #     result = futures[i].result()
+    #     if result is not None:
+    #         incorrect_encoding_files.append(result)
+
+    # with ThreadPoolExecutor(max_workers=1) as executor:
+    #     results = list(executor.map(check_encoding, xml_files))
+
+    # incorrect_encoding_files = [result for result in results if result is not None]
+
+    incorrect_encoding_files = []
+    for i, file in enumerate(xml_files):
+        # print(f"Waiting for future {i}")
+        result = check_encoding(file, i)
+        if result is not None:
+            incorrect_encoding_files.append(result)
+
+    print(" " * 80)
+
     if len(incorrect_encoding_files) == 0:
         print(Fore.GREEN + "No suspicious files. All good")
         return
 
     table = PrettyTable()
-    table.field_names = ["File", "Encoding"]
+    table.field_names = ["File", "Guessed Encoding", "Compatible"]
     table.align = "l"
 
     unique_files = set(incorrect_encoding_files)
+
     sorted_files = sorted(unique_files, key=lambda x: x[1])
-    for file_path, encoding in sorted_files:
-        table.add_row([file_path, encoding])
+    for file_path, encoding, compatible in sorted_files:
+        if compatible:
+            compatible_str = Fore.GREEN + "Yes" + Fore.RESET
+        else:
+            compatible_str = Fore.RED + "No!" + Fore.RESET
+
+        table.add_row([file_path, encoding, compatible_str])
 
     print(table)
-    print(f"There are {len(incorrect_encoding_files)} suspicious files.")
+    print(f"There are {len(unique_files)} suspicious files.")
 
 
 def main():
