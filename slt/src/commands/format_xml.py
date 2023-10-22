e50e7009312ac28c8ab13a8d0ac0c7ed5a0f7722
HalavicH <HalavicH@gmail.com>
2023-10-22 14:22:33 +0200

    text: xml: Fix format XML after refactoring

--- slt/src/commands/format_xml.py
+++ slt/src/commands/format_xml.py
@@ -1,7 +1,8 @@
 from src.commands.common import get_xml_files_and_log, process_files_with_progress
 from src.utils.colorize import cf_cyan, cf_green
 from src.utils.file_utils import read_xml, save_xml
-from src.utils.misc import create_pretty_table
+from src.utils.misc import create_pretty_table, exception_originates_from
+from src.utils.plain_text_utils import format_text_entry
 from src.utils.xml_utils import *
 
 include_example = cf_red('#include "some/other/file.xml"')
@@ -35,54 +36,68 @@ def format_xml_text_entries(text_formatted_xml, indent_level) -> (str, bool):
     return updated_xml_str.strip() + "\n", was_formatted
 
 
-def process_file(file_path, result, args):
-    fix_errors = args.fix
-    format_text = args.format_text_entries
-
+def process_file(file_path, results, args):
     was_fixed = False
     text_was_formatted = False
     was_formatted = False
 
     try:
-        with open(file_path, 'r', encoding=windows_1251) as file:
+        # Try to open the file to check if it's readable
+        with open(file_path, 'r', encoding='windows_1251') as file:
             file.read()
-    except UnicodeDecodeError as e:
-        msg = f"Can't open file {file_path} as {windows_1251} encoded. Error: {e}"
-        log_and_save_error(file_path, msg)
 
-    xml_string = read_xml(file_path)
+        # Read XML from the file
+        xml_string = read_xml(file_path)
 
-    # Fix errors
-    fixed_xml = xml_string
-    if fix_errors:
-        fixed_xml = fix_possible_errors(xml_string, file_path)
-        if fixed_xml != xml_string:
-            log.debug("Fixed typical XML errors")
-            was_fixed = True
+        # Fix errors if needed
+        fixed_xml = xml_string
+        if args.fix:
+            fixed_xml = fix_possible_errors(xml_string, file_path)
+            if fixed_xml != xml_string:
+                log.debug("Fixed typical XML errors")
+                was_fixed = True
+
+        # Format the document
+        formatted = format_xml_string(fixed_xml, file_path)
+        if formatted != fixed_xml:
+            log.debug("Formatted XML schema")
+            was_formatted = True
+
+        # Optionally format text entries
+        text_formatted_xml = formatted
+        if args.format_text_entries:
+            text_formatted_xml, any_entry_formatted = format_xml_text_entries(text_formatted_xml, tabwidth * 3)
+            if any_entry_formatted:
+                log.debug("Formatted <text> entries")
+                text_was_formatted = True
+
+        # Save the XML back to the file
+        save_xml(file_path, text_formatted_xml)
+
+        if any([was_fixed, text_was_formatted, was_formatted]):
+            was_fix_status = to_yes_no(was_fixed) if args.fix else cf_cyan("Disabled")
+            was_text_form_status = to_yes_no(text_was_formatted) if args.format_text_entries else cf_cyan("Disabled")
+            formatted_status = to_yes_no(was_formatted)
+            results.append((file_path, was_fix_status, was_text_form_status, formatted_status))
+
+    except Exception as e:
+        if args.fix and exception_originates_from("fix_possible_errors", e):
+            was_fix_status = error_str
         else:
-            log.debug("No issues were present")
+            was_fix_status = cf_cyan("Disabled") if not args.fix else "No"
 
-    # Format document
-    formatted = format_xml_string(fixed_xml, file_path)
-    if formatted != fixed_xml:
-        log.debug("Formatted XML schema")
-        was_formatted = True
-    else:
-        log.debug("No formatting was required")
-
-    # Format text entries
-    text_formatted_xml = formatted
-    if format_text:
-        text_formatted_xml, any_entry_formatted = format_xml_text_entries(text_formatted_xml, tabwidth * 3)
-        if any_entry_formatted:
-            log.debug("Formatted <text> entries")
-            text_was_formatted = True
+        if args.format_text_entries and exception_originates_from("format_xml_text_entries", e):
+            was_text_form_status = error_str
         else:
-            log.debug("No <text> entry formatting issues were present")
+            was_text_form_status = cf_cyan("Disabled") if not args.format_text_entries else "-"
 
-    save_xml(file_path, text_formatted_xml)
+        if was_text_form_status == error_str:
+            formatted_status = cf_cyan("N/a")
+        else:
+            formatted_status = error_str
 
-    return was_fixed, text_was_formatted, was_formatted
+        results.append((file_path, was_fix_status, was_text_form_status, formatted_status))
+        log_and_save_error(file_path, f"Unhandled error {e}")
 
 
 def to_yes_no(b: bool) -> str:
