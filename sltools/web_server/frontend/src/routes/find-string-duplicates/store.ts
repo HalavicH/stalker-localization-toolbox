0e5018f53ce6f3d6aa73ef667507a522cae7114f
HalavicH <HalavicH@gmail.com>
2023-12-02 20:00:48 +0100

    front: Fix tooltip

--- sltools/web_server/frontend/src/routes/find-string-duplicates/store.ts
+++ sltools/web_server/frontend/src/routes/find-string-duplicates/store.ts
@@ -3,5 +3,12 @@ import {type Writable, writable} from 'svelte/store';
 
 export const status: Writable<string> = writable("Status bar");
 export const details: Writable<string> = writable("Click on any node/link to see the details");
-export const tooltipData: Writable<string> = writable("");
-export const tooltipPos: Writable<string> = writable("");
+
+export interface TooltipState {
+    posX: number,
+    posY: number,
+    html: string,
+    visible: boolean
+}
+
+export const tooltip: Writable<Partial<TooltipState>> = writable();
