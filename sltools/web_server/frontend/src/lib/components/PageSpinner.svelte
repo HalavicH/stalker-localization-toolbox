b09b3d66953759af0ee7af345fbe8bd6e4fe800b
HalavicH <HalavicH@gmail.com>
2023-12-02 14:38:40 +0100

    front: Add shutdown & spinner

---
+++ sltools/web_server/frontend/src/lib/components/PageSpinner.svelte
@@ -0,0 +1,54 @@
+
+<style>
+    .loading-message {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        background-color: rgba(0, 0, 0, 0.2);
+        padding: 30px;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        z-index: 9999;
+    }
+
+    img {
+        width: 200px;
+        height: 200px;
+    }
+
+    .loading-symbol {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        animation: rotation 2s linear infinite, pulse 1.5s infinite;
+    }
+
+    p {
+        font-size: 20pt;
+        color: #fff; /* Text color */
+        margin: 20px 20px 10px;
+    }
+
+    @keyframes pulse {
+        0% {
+            transform: scale(1);
+            rotate: 0deg;
+        }
+        50% {
+            transform: scale(1.1);
+        }
+        100% {
+            transform: scale(1);
+            rotate: 360deg;
+        }
+    }
+</style>
+
+<div class="loading-message">
+    <div class="loading-symbol">
+        <img alt="" class="radiation-symbol" src="/common/radiation.svg"/>
+    </div>
+    <p>Loading...</p>
+</div>
---
+++ sltools/web_server/frontend/src/lib/components/Shutdown.svelte
@@ -0,0 +1,38 @@
+<!-- HTML -->
+<div on:click={handleClick} on:keydown={undefined} role="button" tabindex="0">
+    <img alt="" {style} src="/common/shutdown.svg">
+</div>
+
+<!-- JS -->
+<script lang="ts">
+    let active = true;
+    $: style = active ? "" : "filter: grayscale(1); box-shadow: none;";
+
+    function handleClick() {
+        active = !active;
+    }
+</script>
+
+<!-- CSS -->
+<style>
+    div {
+        position: fixed;
+        bottom: 20px;
+        left: 0;
+
+        padding: 10px;
+        transition: all 0.2s;
+    }
+
+    img {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+
+        transition: all 0.2s;
+    }
+
+    img:hover {
+        box-shadow: 0 0 10px #ff9c5a;
+    }
+</style>
